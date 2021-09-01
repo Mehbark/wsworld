c6794e2bf6dd162b2d4bd91a889f72c5b1d995c7
Mehbark <clarkemailtest@gmail.com>
2021-09-01 18:55:24 -0400

    oops forgot to remove old folders

--- serverSide/server.js
+++
@@ -1,9 +0,0 @@
-const WebSocketServer = require("ws").Server;
-
-const wss = new WebSocketServer({port: 8080});
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log("Recieved: %s", message);
-  });
-});
